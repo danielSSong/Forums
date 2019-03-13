fa2bb0e4d676fb8dff1800405cc900b1fd704dcc
danielSSong <danielzzang5517@gmail.com>
2019-03-13 13:47:28 +1100

    Create Post Create form

--- Forums.Service/PostService.cs
+++ Forums.Service/PostService.cs
@@ -16,13 +16,11 @@ namespace Forums.Service
         {
             _context = context;
         }
-        public Task Add(Post post)
+        public async Task Add(Post post)
         {
-            throw new NotImplementedException();
+            _context.Add(post);
+            await _context.SaveChangesAsync();
         }
-
-
-
         public Task Delete(int id)
         {
             throw new NotImplementedException();
--- Forums/Controllers/PostController.cs
+++ Forums/Controllers/PostController.cs
@@ -6,6 +6,7 @@ using Forums.Data;
 using Forums.Data.Models;
 using Forums.Models.Post;
 using Forums.Models.Reply;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Forums.Controllers
@@ -13,9 +14,13 @@ namespace Forums.Controllers
     public class PostController : Controller
     {
         private readonly IPost _postService;
-        public PostController(IPost postService)
+        private readonly IForum _forumService;
+        private static UserManager<ApplicationUser> _userManager;
+        public PostController(IPost postService, IForum forumService, UserManager<ApplicationUser> userManager)
         {
             _postService = postService;
+            _forumService = forumService;
+            _userManager = userManager;
         }
         public IActionResult Index(int id)
         {
@@ -34,6 +39,47 @@ namespace Forums.Controllers
                 Replies = replies
             };
             return View(model);
+        }
+
+        public IActionResult Create(int id)
+        {
+            // Note id is Forum.Id
+            var forum = _forumService.GetById(id);
+            var model = new NewPostModel
+            {
+                ForumName = forum.Title,
+                ForumId = forum.Id,
+                ForumImageUrl = forum.ImageUrl,
+                AuthorName = User.Identity.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost(NewPostModel model)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = _userManager.FindByIdAsync(userId).Result;
+            var post = BuildPost(model, user);
+
+            await _postService.Add(post);
+            // TODO: Implement User Rating Management
+
+            return RedirectToAction("Index", "Post", new { id = post.Id });
+        }
+
+        private Post BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            var forum = _forumService.GetById(model.ForumId);
+            return new Post
+            {
+                Title = model.Title,
+                Content = model.Content,
+                Created = DateTime.Now,
+                User = user,
+                Forum = forum
+            };
+
         }
 
         private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
